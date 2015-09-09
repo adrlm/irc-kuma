3f2a2d326c8518e2f765d89df5e19db47426f510
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-08 22:20:20 -0600

    testing

--- src/db-init.py
+++ src/db-init.py
@@ -2,22 +2,12 @@ import sqlite3 as sql
 import sys
 
 
-con = None
+con = sql.connect('living_in_the_.db')
 
-try:
-   con = sql.connect('living_in_the_.db')
-
+with con:
    cur = con.cursor()
    cur.execute('SELECT SQLITE_VERSION()')
 
    data = cur.fetchone()
 
    print("SQLite version {0}".format(data))
-
-except (sql.Error, e):
-   print("Error {0}:".format(e.args[0]))
-   sys.exit(1)
-
-finally:
-   if con:
-      con.close()
