3630ddd8fcfcc9d8b911a781030bfdda4d4e0faf
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-08 23:51:27 -0600

    oops

--- src/irc-kuma.py
+++ src/irc-kuma.py
@@ -228,6 +228,7 @@ IRC command definitions and functions.
 
 def send_help ():
    out = "/me .markov [#animebytes, #mango], .op [user], .deop [user], .getops"
+   send_message(CHAT, out)
 
 def send_markov (chat_name):
    out = gen_markov(chat_name)
