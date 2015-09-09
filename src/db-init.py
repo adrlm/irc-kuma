384f509d8b107f3ac93c872f13e88fe87bda3c59
rekyuu <senkan.re.kyuu@gmail.com>
2015-09-08 22:41:25 -0600

    testing

--- src/db-init.py
+++ src/db-init.py
@@ -6,22 +6,17 @@ con = sql.connect('living_in_the_.db')
 OPS  = ["bo1g", "rekyuu", "Luminarys", "Mei-mei", "nuck", "tsunderella", "Liseda", "Wizzie", "Wizbright"]
 
 with con:
+   con.row_factory = sql.Row
    db = con.cursor()
 
    db.execute('DROP TABLE IF EXISTS Ops;')
    db.execute('CREATE TABLE IF NOT EXISTS Ops(Id INTEGER PRIMARY KEY, Name TEXT);')
 
-   i = 0
    for op in OPS:
       db.execute("INSERT INTO Ops(Name) VALUES('{0}');".format(op))
-      i += 1
 
    db.execute('SELECT * FROM Ops;')
+   rows = db.fetchall()
 
-   while True:
-      row = db.fetchone()
-
-      if row == None:
-         break
-
-      print(row[0], row[1])
+   for row in rows:
+      print(row['Name'])
